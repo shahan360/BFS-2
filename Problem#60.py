86c733eaadc97b49b21f745ae836b76430ea2b1c
shahan360 <sbsb4961@gmail.com>
2025-07-25 01:32:57 -0500

    committed

---
+++ Problem#60.py
@@ -0,0 +1,18 @@
+'''
+199. Binary Tree Right Side View
+Link: https://leetcode.com/problems/binary-tree-right-side-view/
+'''
+
+# Time Complexity : 
+# Space Complexity : 
+# Did this code successfully run on Leetcode : 
+# Any problem you faced while coding this :
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
---
+++ Problem#61.py
@@ -0,0 +1,18 @@
+'''
+993. Cousins in Binary Tree
+Link: https://leetcode.com/problems/cousins-in-binary-tree/
+'''
+
+# Time Complexity : 
+# Space Complexity : 
+# Did this code successfully run on Leetcode : 
+# Any problem you faced while coding this :
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
